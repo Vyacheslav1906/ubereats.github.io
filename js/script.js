82bee1615316e074e46d6e3b03fd310f5d454fe0
Vyacheslav1906 <37370756+Vyacheslav1906@users.noreply.github.com>
2019-08-06 22:44:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	var $page = $('html, body');
+	$('a[href*="#"]').click(function() {
+		$page.animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 400);
+		return false;
+	});
+	
+});
